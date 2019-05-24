b3a7614d1c7da0e3d8c7abdf7e1f1a8ffa937a54
Harshawardhan Natu <harshawardhan.n333@gmail.com>
2019-05-24 12:48:47 +0530

    Create wzx_wrapper.py

---
+++ wzx_wrapper.py
@@ -0,0 +1,11 @@
+import requests, json
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+headers = {'User-Agent': ua.random}
+
+url = "https://api.wazirx.com/api/v2/tickers/json"
+
+content = requests.get(url, headers=headers)
+data = content.json()
+print(data)
