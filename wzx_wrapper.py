54d605642a021cb186c715eca04512ef5da4f88b
Harshawardhan Natu <22957972+HRN-Projects@users.noreply.github.com>
2019-09-29 16:05:16 +0530

    ticker URL updated

--- wzx_wrapper.py
+++ wzx_wrapper.py
@@ -4,7 +4,7 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 headers = {'User-Agent': ua.random}
 
-url = "https://api.wazirx.com/api/v2/tickers/json"
+url = "https://api.wazirx.com/api/v2/tickers"
 
 content = requests.get(url, headers=headers)
 data = content.json()
